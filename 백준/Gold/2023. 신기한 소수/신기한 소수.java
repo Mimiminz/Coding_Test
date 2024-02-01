7fd7f9bccf014f1d42c99929bc3972750065a86a
MinJu Jo <pida6716@gmail.com>
2024-02-01 17:35:39 +0900

    [Gold V] Title: 신기한 소수, Time: 120 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -7,9 +7,9 @@ import java.io.InputStreamReader;
  *  @author JOMINJU
  *  
  *  1. 해당 숫자가 소수인지 판별하는 함수를 작성한다.
- *  2. 소수가 아니라면 함수를 즉시 리턴한다.
+ *  2. 소수가 아니라면 함수를 다음으로 넘긴다
  *  3. 소수라면 다음수를 넣고 다음 자리 소수 판별을 시작한다.
- *  4. N번째 자리까지 도달한다면, 값을 리턴하고 함수를 종료한다.
+ *  4. N번째 자리까지 도달한다면, 값을 리턴하고 다음 수를 확인한다.
  */
 
 public class Main {
@@ -18,30 +18,42 @@ public class Main {
 	
 	public static int maxNumber;
 	
-	public static void checkDecimal(int current) {
+	public static boolean checkDecimal(int num) {
 		
-		for(int idx = 1; idx < 10; idx++) {
-			int checkNum = current*10 + idx;
-			int flag = 0;
-			if(checkNum == 1)	continue;
-			
-			for(int check = 2; check <= (checkNum >> 1); check++) {
-				if(checkNum % check == 0) {
-	            	// 2. 소수가 아니라면 함수를 즉시 리턴한다.
-					flag = 1;
-					break;
-				}
+		if(num == 1)
+			return false;
+	    
+	    // n의 제곱근까지 나눈다.
+	    for(int idx = 2; idx <= Math.sqrt(num); idx++) {   
+	        // 소수라면 뒤에 오는 코드를 스킵한다.
+	    	if(num % idx == 0) {
+				return false;
 			}
-			if(flag == 1)
+		}
+	    
+	    return true;
+	}
+	
+	public static void makeNum(int current) {
+		
+		// 판별할 수 만들기
+		for(int idx = 1; idx < 10; idx++) {
+			// 한자리 늘리기
+			int checkNum = current*10 + idx;
+			
+			boolean isPrime = checkDecimal(checkNum);
+			
+			// 값이 소수가 아니라면 넘어가기
+			if(isPrime == false)
 				continue;
 			
-			// 4. N번째 자리까지 도달한다면, 값을 리턴하고 함수를 종료한다.
+			// 4. N번째 자리까지 도달한다면, 값을 리턴하고 다음 수를 확인한다.
 			if(Integer.toString(checkNum).length() == maxNumber) {
 				sb.append(checkNum).append("\n");
 				continue;
 			}
 			// 3. 소수라면 다음수를 넣고 다음 자리 소수 판별을 시작한다.
-			checkDecimal(checkNum);
+			makeNum(checkNum);
 			
 		}
 		
@@ -52,7 +64,7 @@ public class Main {
 		sb = new StringBuilder();
 		maxNumber = Integer.parseInt(br.readLine().trim());
 		
-		checkDecimal(0);
+		makeNum(0);
 		System.out.println(sb);
 		br.close();
 	}
