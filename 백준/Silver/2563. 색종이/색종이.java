c7aeb4d18186d4f753042c43afa0167c1bf2accf
MinJu Jo <pida6716@gmail.com>
2024-02-07 14:32:30 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -18,30 +18,33 @@ public class Main {
 	public static StringBuilder sb;
 	public static StringTokenizer st;
 	
-	public static int[][] location = new int[100][100];
-	public static int size;
+	public final static int MAX_SIZE = 100;
 	
-	public static void paintColor(int row, int col) {
+	public static boolean[][] location;
+	
+	public static int paintColor(int row, int col, int size) {
 		for(int y = col; y < col + 10; y++) {
 			for(int x = row; x < row + 10; x++) {
-				if(location[y][x] != 1) {
-					location[y][x] = 1;
+				if(!location[y][x]) {
+					location[y][x] = true;
 					size++;
 				}
 			}
 		}
+		return size;
 	}
 
 	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
 		int num = Integer.parseInt(br.readLine().trim());
-		size = 0;
+		location = new boolean[MAX_SIZE][MAX_SIZE];
+		int size = 0;
 		for(int idx = 0; idx < num; idx++) {
 			st = new StringTokenizer(br.readLine().trim());
 			int row = Integer.parseInt(st.nextToken());
 			int col = Integer.parseInt(st.nextToken());
-			paintColor(row, col);
+			size = paintColor(row, col, size);
 		}
 		sb.append(size);
 		System.out.println(sb);
