664b76e56eb057e0bf8e5e61cedc32cbcc3e1fbe
MinJu Jo <pida6716@gmail.com>
2024-02-14 13:27:49 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -7,7 +7,10 @@ import java.util.StringTokenizer;
  * BOJ 1074 Z
  * @author JOMINJU
  * 
- *	1. 함수 안에서 Z의 형태로 row값과 col값을 변경시킨다
+ *	1. 크기의 절반만큼 나눴을 때 왼쪽 상단, 오른쪽 상단, 왼쪽 하단, 오른쪽 하단에 따라 카운트의 값을 변경시킨다.
+ *	2. 이후 구해야하는 row와 col의 값도 이동한 만큼 감소시킨다.
+ *	3. 하나의 Z를 가질 수 있는, 크기가 2인 값이 될때까지 반복한다.
+ *	4. 크기가 2일 경우 row와 col의 위치에 따라 카운트 값을 변경시킨다.
  *	2. 이후 2의 n-1의 제곱이 될 때마다 재귀를 호출시킨다
  *	3. 입력받은 row, col을 몇번째로 방문하는지 확인한다.
  */
@@ -23,15 +26,14 @@ public class Main {
 	
 	public static void findLocation(int currentN, int count) {
 		if(currentN == 2) {
-			if(rowNum == 0 && colNum == 0) {
-				sb.append(count);
-			}else if(rowNum == 1 && colNum == 0) {
-				sb.append(count+1);
+			if(rowNum == 1 && colNum == 0) {
+				count += 1;
 			}else if(rowNum == 0 && colNum == 1) {
-				sb.append(count+2);
-			}else {
-				sb.append(count+3);
+				count += 2;
+			}else if(rowNum == 1 && colNum == 1){
+				count += 3;
 			}
+			sb.append(count);
 			return;
 		}
 		
@@ -56,8 +58,7 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		colNum = Integer.parseInt(st.nextToken());
 		rowNum = Integer.parseInt(st.nextToken());
-		int size = (int) Math.pow(2, N);
-		findLocation(size, 0);
+		findLocation((int) Math.pow(2, N), 0);
 		System.out.println(sb);
 		br.close();
 	}
